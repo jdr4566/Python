1dc77e8b229bb946be37472236caf28f30587eef
JDR4566 <89748314+jdr4566@users.noreply.github.com>
2021-10-07 15:53:25 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+"""
+This is a Multi-line comment and  test program, created in VS Code 10/7/21
+"""
+
+#   This is a single line comment
+
+
+#   Defining Variables
+
+carname = "Volvo"
+
+x = 5
+y = 10
+
+# Defining more variables!!
+
+a = "Python is "
+b = "awesome"
+c= a+b
+print("The sum of x and y is ")
+print(x + y)
+
+
+# if statements
+if 5 > 2:
+ print("five is greater than two!!!")
+
+
+#Printing variable cc
+print(c)
+
+
+
